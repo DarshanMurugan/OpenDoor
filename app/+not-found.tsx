b5c0a3067f889d6c666da6501ef1b21ae9c292b3
DarshanMurugan <darshanmurugan56@gmail.com>
2026-06-22 13:34:23 +0530

    OpenDoor
    inital commit for OpenDoor projecta

---
+++ app/+not-found.tsx
@@ -0,0 +1,32 @@
+import { View, StyleSheet } from 'react-native';
+import { Link, Stack } from 'expo-router';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops! Page not found!' }} />
+      <View style={styles.container}>
+        <Link href="/" style={styles.button}>
+          go to home
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color: '#fff',
+  },
+
+
+});
